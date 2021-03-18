37ddbea4c4b62801cb12d759de7bbcef0e402c3a
naraxus <fe.siegmeier@gmail.com>
2021-03-18 21:32:37 +0100

    messing with functions of oled and timegetting

--- main.py
+++ main.py
@@ -28,6 +28,52 @@ def get_temp(): # Temperaturen auslesen -> ermittelt alle 1 Minute die Temperatu
     temp_rohr = ds_sensor.read_temp(roms[1])
     print(temp_tank)
 
+def get_time():
+     # if lose wifi connection, reboot ESP8266
+    if not wifi.isconnected():
+        machine.reset()
+    
+    # query and get web JSON every web_query_delay ms
+    if utime.ticks_ms() - update_time >= web_query_delay:
+    
+        # HTTP GET data
+        response = urequests.get(url)
+    
+        if response.status_code == 200: # query success
+        
+            print("JSON response:\n", response.text)
+            
+            # parse JSON
+            parsed = response.json()
+            datetime_str = str(parsed["datetime"])
+            year = int(datetime_str[0:4])
+            month = int(datetime_str[5:7])
+            day = int(datetime_str[8:10])
+            hour = int(datetime_str[11:13])
+            minute = int(datetime_str[14:16])
+            second = int(datetime_str[17:19])
+            subsecond = int(round(int(datetime_str[20:26]) / 10000))
+        
+            # update internal RTC
+            rtc.datetime((year, month, day, 0, hour, minute, second, subsecond))
+            update_time = utime.ticks_ms()
+            print("RTC updated\n")
+   
+        else: # query failed, retry retry_delay ms later
+            update_time = utime.ticks_ms() - web_query_delay + retry_delay
+    
+    # generate formated date/time strings from internal RTC
+    date_str = "Date: {1:02d}/{2:02d}/{0:4d}".format(*rtc.datetime())
+    time_str = "Time: {4:02d}:{5:02d}:{6:02d}".format(*rtc.datetime())
+
+def show_time():
+    # update SSD1306 OLED display
+    oled.fill(0)
+    oled.text("Aktuelle Uhrzeit: ", 0, 5)
+    oled.text(date_str, 0, 15)
+    oled.text(time_str, 0, 45)
+    oled.show()
+
 
 # user data
 ssid = "WLAN-589392" # wifi router name
@@ -69,49 +115,5 @@ update_time = utime.ticks_ms() - web_query_delay
 
 # main loop
 while True:
-    
-    # if lose wifi connection, reboot ESP8266
-    if not wifi.isconnected():
-        machine.reset()
-    
-    # query and get web JSON every web_query_delay ms
-    if utime.ticks_ms() - update_time >= web_query_delay:
-    
-        # HTTP GET data
-        response = urequests.get(url)
-    
-        if response.status_code == 200: # query success
-        
-            print("JSON response:\n", response.text)
-            
-            # parse JSON
-            parsed = response.json()
-            datetime_str = str(parsed["datetime"])
-            year = int(datetime_str[0:4])
-            month = int(datetime_str[5:7])
-            day = int(datetime_str[8:10])
-            hour = int(datetime_str[11:13])
-            minute = int(datetime_str[14:16])
-            second = int(datetime_str[17:19])
-            subsecond = int(round(int(datetime_str[20:26]) / 10000))
-        
-            # update internal RTC
-            rtc.datetime((year, month, day, 0, hour, minute, second, subsecond))
-            update_time = utime.ticks_ms()
-            print("RTC updated\n")
-   
-        else: # query failed, retry retry_delay ms later
-            update_time = utime.ticks_ms() - web_query_delay + retry_delay
-    
-    # generate formated date/time strings from internal RTC
-    date_str = "Date: {1:02d}/{2:02d}/{0:4d}".format(*rtc.datetime())
-    time_str = "Time: {4:02d}:{5:02d}:{6:02d}".format(*rtc.datetime())
-
-    # update SSD1306 OLED display
-    oled.fill(0)
-    oled.text("ESP8266 WebClock", 0, 5)
-    oled.text(date_str, 0, 25)
-    oled.text(time_str, 0, 45)
-    oled.show()
-    
-    utime.sleep(0.1)
+    get_time()
+    show_time()
