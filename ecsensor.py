382179dada5a62803833128bb451745136f83ffe
fesiegmeier <fe.siegmeier@gmail.com>
2021-03-27 11:04:25 +0100

    Update

--- ecsensor.py
+++ ecsensor.py
@@ -1,4 +1,10 @@
-from machine import Pin
+from machine import Pin, ADC
 
 class ecsensor():
     def __init__(self, Power, GND, Data, R, Temp):
+        self.ECPower = Pin(Power, Pin.OUT)
+        self.ECPower.value(0)
+        self.ECGND.value(0)
+        self.ECGND = Pin(GND, Pin.OUT)
+        self.ECData = ADC(Pin(Data))
+        self.ECData.atten(ADC.ATTN_11DB)
