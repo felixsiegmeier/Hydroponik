800f88e235848b66d0ab5870033d1c69b89cc121
naraxus <fe.siegmeier@gmail.com>
2021-03-27 20:00:31 +0100

    pep08

--- ecsensor.py
+++ ecsensor.py
@@ -1,32 +1,30 @@
 from machine import Pin, ADC
 
-class ecsensor():
-    def __init__(self, Power, GND, Data, R, Temp, Controller, max, min):
-        self.ECPower = Pin(Power, Pin.OUT)
-        self.ECPower.value(0)
-        self.ECGND.value(0)
-        self.ECGND = Pin(GND, Pin.OUT)
-        self.ECData = ADC(Pin(Data))
-        self.ECData.atten(ADC.ATTN_11DB)
-        self.Resistor = R
-        self.Temp = Temp
-        self.Rmax = max
-        self.Rmin = min
-        if Controller == "ESP32":
-            self.Rc = 25
+class EcSensor():
+    def __init__(self, power, data, resistor, temp, controller, max, min):
+        self.ec_power = Pin(power, Pin.OUT)
+        self.ec_power.value(0)
+        self.ec_data = ADC(Pin(data))
+        self.ec_data.atten(ADC.ATTN_11DB)
+        self.resistor = resistor
+        self.temp = temp
+        self.r_max = max
+        self.r_min = min
+        if controller == "ESP32":
+            self.pin_resistance = 25
             self.range = 4095
             self.voltage = 3.3
         if Controller == "ESP8266":
-            self.Rc = 25
+            self.pin_resistance = 25
             self.range = 1023
             self.voltage = 3.3
 
-    def measureEC(self):
-        raw = self.ECData.read()
-        Vdrop = self.voltage*self.raw/self.range
-        Resistance = (Vdrop * self.Resistor) / (self.voltage - Vdrop)
-        Resistance = Resistance - self.Rc
-        Resistance25 = Resistance/(1+(0.019*(self.Temp-25)))
+    def ec_measure(self):
+        raw = self.ec_data.read()
+        v_drop = self.voltage*self.raw/self.range
+        Resistance = (v_drop * self.resistor) / (self.voltage - v_drop)
+        resistance = resistance - self.pin_resistance
+        resistance_25 = resistance/(1+(0.019*(self.temp-25)))
 
 
 
