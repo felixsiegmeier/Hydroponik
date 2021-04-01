2875d56c8a9ee4244b38e15a90c56729c3a7ff6f
naraxus <fe.siegmeier@gmail.com>
2021-04-01 21:58:27 +0200

    Update

--- button.py
+++ button.py
@@ -1,21 +1,32 @@
-#Interrupt-Handler
-# übergeben werden Pin und Zahl, bis  zu der gezählt wird
-''' button = Button(17,5) erzeugt einen PULL_DOWN-Button auf Pin 17 mit Aktivierung auf Druck, 
-der von 0 bis 5 aufwärts zählt und dann wieder bei 0 beginnt '''
 from machine import Pin
 
 class Button():
-    def __init__(self, button_pin, counter_range):
-        self.counter = 0 #gibt den Zähler wieder
-        self.pin = Pin(button_pin, Pin.IN, Pin.PULL_DOWN)
-        self.range = counter_range
-        self.pin.irq(trigger=Pin.IRQ_RISING, handler=self.update_counter)
+    def __init__(self, oled_button_pin, licht_button_pin, oled_modus_counter_max, licht_modus_counter_max, oled_modus_licht):
+        self.oled_modus_counter = 0
+        self.licht_modus_counter = 0 
+        self.oled_pin = Pin(oled_button_pin, Pin.IN, Pin.PULL_DOWN)
+        self.licht_pin = Pin(licht_button_pin, Pin.IN, Pin.PULL_DOWN)
+        self.oled_modus_counter_max = oled_modus_counter_max
+        self.licht_modus_counter_max = licht_modus_counter_max
+        self.oled_modus_licht = oled_modus_licht
+        self.oled_pin.irq(trigger=Pin.IRQ_RISING, handler=self.oled_handler)
+        self.licht_pin.irq(trigger=Pin.IRQ_RISING, handler=self.licht_handler)
     
-    def update_counter(self, arg):
-        if self.counter < self.range:
-            self.counter += 1
+    def oled_handler(self, arg):
+        if self.oled_modus_counter < self.oled_modus_counter_max:
+            self.oled_modus_counter += 1
         else:
-            self.counter = 0
+            self.oled_modus_counter = 0
 
-    def value(self):
-        return (self.counter)
+    def licht_handler(self, arg):
+        self.oled_modus_counter = self.oled_modus_licht
+        if self.licht_modus_counter < self.licht_modus_counter_max:
+            self.licht_modus_counter += 1
+        else:
+            self.licht_modus_counter = 0
+
+    def get_oled_modus(self):
+        return (self.oled_modus_counter)
+
+    def get_licht_modus(self):
+        return (self.licht_modus_counter)
